12d6fcbe7a343a3818cb78ff3f8730bdcbb4a95f
Nicolas Mazitelli <nicolas.mazitelli@mercadolibre.com>
2020-04-19 03:12:26 -0300

    ui chetoide

--- scenes/ui_control.gd
+++ scenes/ui_control.gd
@@ -1,20 +1,21 @@
 extends Control
 onready var main = get_parent()
-var distance_bar 
-var speed_bar 
+onready var speed_percent 
 var energy_bar 
+var speed_needle
+var needle_rotation setget set_needle_rotation
 
 func _ready():
-	distance_bar = get_node("distance/distance_bar")
 	energy_bar = get_node("speed/energy_bar")
-	speed_bar = get_node("speed/speed_bar")
-	
-	distance_bar.max_value = main.distance_to_goal
-	speed_bar.max_value = main.max_speed
+	speed_needle = get_node("speed/flecha")
 	energy_bar.max_value = main.max_energy
-	
+
 func _process(delta):
-	distance_bar.value = main.current_distance
-	speed_bar.value = main.current_speed
 	energy_bar.value = main.current_energy
+	speed_percent = main.current_speed * 100 / main.max_speed
+	set_needle_rotation((speed_percent * 167 / 100) - 157)
+	speed_needle.rotation_degrees = needle_rotation
+	
+func set_needle_rotation(value):
+	needle_rotation = clamp(value, -157, 10)
 	
